a773ee202dd4217ed6457ee761ddd05360f8574a
yazbekw <yazbek965@gmail.com>
2025-10-04 12:53:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-[file name]: main (1).py
-[file content begin]
 from fastapi import FastAPI, HTTPException
 import httpx
 import asyncio
@@ -665,4 +663,3 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
-[file content end]
