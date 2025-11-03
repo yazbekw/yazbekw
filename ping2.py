d2b11561e6d4c66987bdd90857538312c63ac581
yazbekw <yazbek965@gmail.com>
2025-11-03 17:05:40 +0300

    Update ping2.py

--- ping2.py
+++ ping2.py
@@ -13,6 +13,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 URLS = [
     "https://ping1-5j34.onrender.com", 
     "https://ping-397j.onrender.com",
+    "https://eth-dhvl.onrender.com",
     "https://scanner-8ika.onrender.com",
 ]
 
