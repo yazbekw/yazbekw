a2751c2064a7861276d72e7c6c25845e2d66ff6d
yazbekw <yazbek965@gmail.com>
2025-11-22 20:11:11 +0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -16,6 +16,7 @@ URLS = [
     "https://monitor-oqk7.onrender.com",
     "https://buy-scanner.onrender.com",
     "https://eth-dhvl.onrender.com",
+    "https://scanner-iae7.onrender.com",
     "https://scanner-8ika.onrender.com",
 ]
 
